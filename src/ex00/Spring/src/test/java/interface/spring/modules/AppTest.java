9b4affeb0680bb50ea9f8d00fc87d2edc8e117f5
Macy Gabrielle <macygabr@ob-f2.nsk.21-school.ru>
2024-03-10 21:22:09 +0700

    backup

---
+++ src/ex00/Spring/src/test/java/interface/spring/modules/AppTest.java
@@ -0,0 +1,20 @@
+package interface.spring.modules;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
