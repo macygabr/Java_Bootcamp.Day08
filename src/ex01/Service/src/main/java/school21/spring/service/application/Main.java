59cf4ad992078eeebf4673e1c6a8d6dd537bda61
Macy Gabrielle <macygabr@ob-f2.nsk.21-school.ru>
2024-03-11 13:56:29 +0700

    start ex01

---
+++ src/ex01/Service/src/main/java/school21/spring/service/application/Main.java
@@ -0,0 +1,13 @@
+package school21.spring.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
